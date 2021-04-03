2f4d9da027fb57232a8e425daa9461bd719a20d9
smfssilva <smfssilva@yahoo.com>
2021-04-03 19:12:50 -0300

    DEBUG FALSE

--- fusion/settings.py
+++ fusion/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '*m0^6wr9s2yf1*pqcb!j+4mbpv1*jloo)ww84hr5t-@7+nukry'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
